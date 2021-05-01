47fe13536aeb025bec442e6619fb5939a4753070
Abduvohid Mirzamahmudov <abduvohid.mirzamahmudov@gmail.com>
2021-05-01 10:48:29 +0500

    test2

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('test/{parametr?}', function ($a1=false) {
-    return "qalesan".$a1;
+Route::get('test/{parametr?}', function ($a=false) {
+    return "qalesan".$a;
 });
