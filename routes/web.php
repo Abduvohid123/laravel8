a54e9b637f0d6b8a2056ebf71be5b0286fb193ed
Abduvohid Mirzamahmudov <abduvohid.mirzamahmudov@gmail.com>
2021-05-08 12:11:47 +0500

    404 not found

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('til/{language}',function ($language){
-    \Illuminate\Support\Facades\App::setLocale($language);
-    return __('message.lorem');
-});
+
